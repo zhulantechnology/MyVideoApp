dfc0d52995ad676db9f698ae3c389c4bbd72d302
Jeffreywangjun <wangjunahasd@163.com>
2018-05-30 16:57:08 +0800

    add vuandroidadsdk module & make app depend it ok
    Signed-off-by: Jeffreywangjun <wangjunahasd@163.com>

--- app/src/main/java/com/jeffrey/view/fragment/home/HomeFragment.java
+++ app/src/main/java/com/jeffrey/view/fragment/home/HomeFragment.java
@@ -1,11 +1,13 @@
 package com.jeffrey.view.fragment.home;
 
 import android.content.Context;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.jeffrey.activity.R;
 import com.jeffrey.view.fragment.BaseFragment;
@@ -18,6 +20,7 @@ public class HomeFragment extends BaseFragment {
 
     private View mContentView;
     private Context mContext;
+    private ImageView mLoadingView;
 
     public HomeFragment() {
 
@@ -33,6 +36,14 @@ public class HomeFragment extends BaseFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         mContext = getActivity();
         mContentView = inflater.inflate(R.layout.fragment_home_layout, container, false);
+        initView();
         return mContentView;
+    }
+
+    private void initView() {
+
+        mLoadingView = (ImageView) mContentView.findViewById(R.id.loading_view);
+        AnimationDrawable anim = (AnimationDrawable) mLoadingView.getDrawable();
+        anim.start();
     }
 }
