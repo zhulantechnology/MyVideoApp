934376f6dad0109c2fdb62bee77a0698cd0e4af0
Jeffreywangjun <wangjunahasd@163.com>
2018-10-09 23:53:43 +0800

    shareSDK
    Signed-off-by: Jeffreywangjun <wangjunahasd@163.com>

---
+++ app/src/main/java/com/jeffrey/share/ShareData.java
@@ -0,0 +1,44 @@
+package com.jeffrey.share;
+
+import cn.sharesdk.framework.Platform;
+
+/**
+ * 要分享的数据实体
+ * Created by Jun.wang on 2018/10/9.
+ */
+
+public class ShareData {
+    // 要分享到的平台
+    public ShareManager.PlatformType mPlatformType;
+    // 要分享到的平台的参数
+    public Platform.ShareParams mShareParams;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ app/src/main/java/com/jeffrey/share/ShareDialog.java
@@ -0,0 +1,8 @@
+package com.jeffrey.share;
+
+/**
+ * Created by Jun.wang on 2018/10/9.
+ */
+
+public class ShareDialog {
+}
