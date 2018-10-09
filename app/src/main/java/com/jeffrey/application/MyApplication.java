e53ffb9af3af2c0feaa5f99dfeb06661193ceb59
Jeffreywangjun <wangjunahasd@163.com>
2018-10-09 20:13:14 +0800

    ShareSDK 分享功能
    Signed-off-by: Jeffreywangjun <wangjunahasd@163.com>

--- app/src/main/java/com/jeffrey/application/MyApplication.java
+++ app/src/main/java/com/jeffrey/application/MyApplication.java
@@ -22,6 +22,10 @@ public class MyApplication extends Application {
         AdSDKManager.init(this);
     }
 
+    private void initShareSDK() {
+        ShareManager.initSDK(this);
+    }
+
     public static MyApplication getInstance() {
         return mApplication;
     }
